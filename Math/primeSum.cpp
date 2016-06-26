dfc5a481c8576dcddf206126c80d9d333cffa96b
rrichajalota <rrichajalota@gmail.com>
2016-06-26 12:54:44 +0530

    solution of primeSum added

---
+++ Math/primeSum.cpp
@@ -0,0 +1,51 @@
+/*  PRIME SUM
+    Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
+    NOTE A solution will always exist. read Goldbach’s conjecture
+    Example:
+    Input : 4
+    Output: 2 + 2 = 4
+    If there are more than one solutions possible, return the lexicographically smaller solution.
+    If [a, b] is one solution with a <= b,
+    and [c,d] is another solution with c <= d, then
+    [a, b] < [c, d] 
+    If a < c OR a==c AND b < d. 
+*/
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
+
+
+vector<int> Solution::primesum(int A) {
+    vector<bool> prime(A,true);
+    
+    prime[0]=prime[1]=false;
+    
+    for(int i=2; i < A; i++){
+        
+        if(prime[i]){
+            int j= i;
+            while(j < A && j+i < A){
+                prime[j+i]= false;
+                j= j+i;
+            }
+        }
+    }
+    
+    int first=0, second=0;
+    for (int p=2; p < A; p++){
+        if(prime[p]==true && prime[A-p]==true){
+            if(!first && !second){
+                first= p;
+                second= A-p;
+            }
+        }
+    }
+    vector<int> result;
+    result.push_back(first);
+    result.push_back(second);
+    return result;
+}
+
+
+
