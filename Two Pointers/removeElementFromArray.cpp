69bbc27344099046da6145204bf278e89a5e3918
rrichajalota <rrichajalota@gmail.com>
2016-07-03 13:30:46 +0530

    added the solution of removeElementFromArray

---
+++ Two Pointers/removeElementFromArray.cpp
@@ -0,0 +1,23 @@
+/*	Remove Element from Array
+
+	Given an array and a value, remove all the instances of that value in the array. 
+	Also return the number of elements left in the array after the operation.
+	It does not matter what is left beyond the expected length.
+
+	Example:
+	If array A is [4, 1, 1, 2, 1, 3]
+	and value elem is 1, 
+	then new length is 3, and A is now [4, 2, 3] 
+	Try to do it in less than linear additional space complexity.
+*/
+
+int Solution::removeElement(vector<int> &A, int B) {
+    int writeIndex=0;
+    for(int i=0; i < A.size(); i++){
+        if(A[i] != B){
+            A[writeIndex++]= A[i];
+        }
+    }
+    A.resize(writeIndex);
+    return writeIndex;
+}
