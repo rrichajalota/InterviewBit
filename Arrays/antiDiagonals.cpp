da96578114347b85d4dd291a481a57e4b7c07852
rrichajalota <rrichajalota@gmail.com>
2016-07-25 20:54:48 +0530

    added the solution of antiDiagonals

---
+++ Arrays/antiDiagonals.cpp
@@ -0,0 +1,42 @@
+/*  Anti-Diagonals
+    Give a N*N square matrix, return an array of its anti-diagonals. Look at the example for more details.
+    Example:
+    Input:  
+
+    1 2 3
+    4 5 6
+    7 8 9
+
+    Return the following :
+    [ 
+      [1],
+      [2, 4],
+      [3, 5, 7],
+      [6, 8],
+      [9]
+    ]
+*/
+
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+    vector<vector<int>> ans;
+    int sum=0;
+    int i=0,j=0;
+    
+    while(sum <= 2*(A.size()-1)){
+        vector<int> row;
+    
+        while(sum == i+j && i < A.size() && j >=0 ){
+            row.push_back(A[i][j]);
+            i= i+1, j= j-1;
+        }
+        
+        ++sum;
+        ans.push_back(row);
+        i=0, j= sum;
+        
+        while(j > A.size()-1){
+            --j, ++i;
+        }
+    }
+    return ans;
+}
