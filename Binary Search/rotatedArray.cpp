73647ddb794a198ecd7e611a9766e642655adfd5
rrichajalota <rrichajalota@gmail.com>
2016-07-28 00:02:21 +0530

    updated rotatedArray

--- Binary Search/rotatedArray.cpp
+++ Binary Search/rotatedArray.cpp
@@ -10,14 +10,23 @@
 int Solution::findMin(const vector<int> &A) {
     int low=0, high= int(A.size()-1);
     int len = A.size();
+    
     while (low <= high) {
-        if (A[low] <= A[high]) return A[low]; // Case 1
+
+        if (A[low] <= A[high]) 
+            return A[low]; 
+
         int mid = (low + high) / 2;
-        int next = (mid + 1) % len, prev = (mid + len - 1) % len;
-        if (A[mid] <= A[next] && A[mid] <= A[prev]) // Case 2
+        int next = (mid + 1) % len, prev = (mid + len - 1) % len;   //circular rotation
+
+        if (A[mid] <= A[next] && A[mid] <= A[prev]) // this will be the minimum element
             return A[mid];
-        else if (A[mid] <= A[high]) high = mid + 1; // Case 3
-        else if (A[mid] >= A[low]) low = mid + 1; // Case 4
+
+        else if (A[mid] <= A[high]) 
+            high = mid + 1; 
+
+        else if (A[mid] >= A[low]) 
+            low = mid + 1; 
     }
     return -1;
 }
