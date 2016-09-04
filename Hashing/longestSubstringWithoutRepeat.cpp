9fe0d4e32fe9aca8306612b3055e934839d20dd5
rrichajalota <rrichajalota@gmail.com>
2016-09-04 10:25:31 +0530

    added linear time solution for longestSubstringWithoutRepeat

--- Hashing/longestSubstringWithoutRepeat.cpp
+++ Hashing/longestSubstringWithoutRepeat.cpp
@@ -31,3 +31,41 @@ int Solution::lengthOfLongestSubstring(string A) {
     }
     return maxcount;
 }
+
+/* Linear Time solution! 
+   Keep track of the starting point of the substring. There can be two cases possible for 
+   a character already present in the hash table. 
+   1) it lies to the left of starting point i.e. it's not a part of our current substring.
+   2) it lies to the right of starting point i.e. it's a part of our current substring. In
+      this case, compare (len, maxlen) and set the starting point as the next index of 
+      the previous occurence of the current character.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        if(s.length()==0)
+            return 0;
+            
+        int i=0, len=0, maxlen=0, start=0;
+        unordered_map<char, int> m;
+        
+        while(i < s.length()){
+
+            if(m.find(s[i]) == m.end() || m[s[i]] < start){
+                m[s[i]] = i;
+                ++len;
+            }
+            else{
+                maxlen = max(len, maxlen);
+                start= m[s[i]] + 1;
+                len = i - m[s[i]];
+                m[s[i]]= i;
+            }
+            ++i;
+        }
+        maxlen= max(maxlen, len);
+        return maxlen;
+    }
+};
